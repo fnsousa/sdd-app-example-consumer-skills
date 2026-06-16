2fc86e91e9130e0a6329d5d3add87520acca7cf6
Felipe Nascimento <felipe_nsousa@outlook.com>
2026-06-16 04:52:15 -0300

    example api

---
+++ src/main/java/com/github/fnsousa/app_example_consumer_skills/AppExampleConsumerSkillsApplication.java
@@ -0,0 +1,13 @@
+package com.github.fnsousa.app_example_consumer_skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExampleConsumerSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExampleConsumerSkillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fnsousa/app_example_consumer_skills/AppExampleConsumerSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fnsousa.app_example_consumer_skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExampleConsumerSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
